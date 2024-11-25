ab19639c8c6ef71b884883fe019bf6da51dcf521
Hung Nguyen <nthungdev@gmail.com>
2024-11-25 05:24:15 -0500

    sample model query

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from app.models import Executive
+
 
 def index(request):
+    executive = Executive.objects.first()
+
+    if executive:
+        print(executive)
+    else:
+        print("Executive not found")
     return HttpResponse("Hello, world.")
