770defade88f402cff92f0f32d719a2bb50d51a7
Hung Nguyen <nthungdev@gmail.com>
2024-12-03 17:27:47 -0500

    add queries descriptions

--- app/queries.py
+++ app/queries.py
@@ -1,7 +1,19 @@
 query_descriptions = {
-    1: 'query 1 description',
-    2: 'query 2 description',
-    3: 'query 3 description',
+    1: 'List all U.S. presidents with their full names and party affiliation.',
+    2: 'List all unique parties represented by legislators.',
+    3: 'Count the total number of U.S. presidents who were elected without party affiliation (party: "no party"), also display their names.',
+    4: 'Identify the longest serving female legislator.',
+    5: 'List all presidents who came to power through succession.',
+    6: 'List presidents and vice presidents grouped by party affiliation.',
+    7: 'List all legislators who served in both the house and the senate.',
+    8: 'Count the number of terms served by each party across all presidents and vice presidents.',
+    9: 'List vice presidents who were appointed rather than elected.',
+    10: 'Identify legislators who did not serve in Congress and later became U.S. presidents.',
+    11: 'Calculate the average duration of presidential terms by party.',
+    12: 'Calculate the total number of years served by each vice president in office.',
+    13: "List legislators who served in Congress during a vice president's term and shared the same party affiliation.",
+    14: 'List presidents who had overlapping terms with legislators in the same state.',
+    15: 'Identify all presidents and vice presidents who served terms under the same party affiliation in both roles.',
 }
 
 def query_1():
