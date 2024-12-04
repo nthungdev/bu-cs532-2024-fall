df8f0dba3cc7c1c5e0fec1466d77f8259ef9ca74
Hung Nguyen <nthungdev@gmail.com>
2024-12-04 13:51:36 -0500

    update raw query 1

--- NoSQL/1.js
+++ NoSQL/1.js
@@ -1,30 +1,44 @@
-use("project2");
-db.executives.aggregate([
-  {
-    $unwind: {
-      path: "$terms",
-    }
-  },
-  {
-    $match: { "terms.type": "prez" }
-  },
-  {
-    $project: {
-      _id: 0,
-      full_name: { 
-        $concat: [ "$name.first", " ", { $ifNull: ["$name.middle", "" ] }, " ", "$name.last" ] 
+use('project2')
+db.executives
+  .aggregate([
+    {
+      $unwind: {
+        path: '$terms',
       },
-      party: "$terms.party",
-      temp_start_date: "$terms.start" 
-    }
-  },
-  {
-    $sort: { temp_start_date: 1 } 
-  },
-  {
-    $project: {
-      full_name: 1,
-      party: 1 
-    }
-  }
-]).toArray();
+    },
+    {
+      $match: {
+        'terms.type': 'prez',
+      },
+    },
+    {
+      $group: {
+        _id: {
+          full_name: {
+            $concat: [
+              '$name.first',
+              ' ',
+              { $ifNull: ['$name.middle', ''] },
+              ' ',
+              '$name.last',
+            ],
+          },
+          party: '$terms.party',
+        },
+        start_date: { $min: '$terms.start' },
+      },
+    },
+    {
+      $sort: {
+        start_date: 1,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        full_name: '$_id.full_name',
+        party: '$_id.party',
+      },
+    },
+  ])
+  .toArray()
